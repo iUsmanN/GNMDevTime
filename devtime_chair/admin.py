664e1cb9ad420d957de1d84c181fc1c722d94db2
= <usmannzr@live.com>
2022-08-19 12:11:37 +0100

    Removed default apps.

--- devtime_chair/admin.py
+++ devtime_chair/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
 from .models import Record
 
 # Register your models here.
 admin.site.register(Record)
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
